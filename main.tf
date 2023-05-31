6a9a5f4b73f9967aada2a4724e52acb11e8530a3
Ashiq Rahman <0xashiqrahman@gmail.com>
2023-05-31 08:34:29 +0530

    main.tf added

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ssm_parameter" "my-amzn-linux-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ssm_parameter.my-amzn-linux-ami.value
+  instance_type = var.instance_type
+}
